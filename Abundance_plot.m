ae10bbf116ba803109a96c1b243d3d3a933e20e8
Jing1215 <131183218+Jing1215@users.noreply.github.com>
2025-09-26 12:08:43 +0800

    Add files via upload
    update abundance with variability

--- Abundance_plot.m
+++ Abundance_plot.m
@@ -22,3 +22,129 @@ c=colorbar;
 xlim([-0.5,cn+1])
 ylim([-0.5,numel(selpert)+1])
 axis equal
+
+%% abundance for S
+figure
+subplot(2,3,1)
+
+selstr = find(ismember(strains.type,[13:20])&strains.Abs_log2median~=0&strains.TFId~=1);
+xv= strains.max_corr(selstr);
+yv = strains.d_log2median(selstr);
+
+hold on
+scatter(xv,yv,40,[0.2588    0.5725    0.7765],'filled')
+[c,p] = corr(xv,yv);
+[x,y] = polyfit(xv,yv,1);
+x_fit = linspace(0,1,100);
+y_fit = polyval(x, x_fit);
+
+colormap(brewermap(1000,'Blues'))
+caxis([0 1])
+
+xlabel('Corr. Reference')
+ylabel('Abandance')
+title(sprintf('S1E: c = %.2f, p = %.2f >0.05*', round(c,1), round(p,1)))
+subplot(2,3,2)
+
+selstr = find(ismember(strains.type,[31:36])&strains.Abs_log2median~=0&strains.TFId~=1);
+xv= strains.max_corr(selstr);
+yv = strains.d_log2median(selstr);
+
+hold on
+scatter(xv,yv,40,[0.2588    0.5725    0.7765],'filled')
+[c,p] = corr(xv,yv);
+[x,y] = polyfit(xv,yv,1);
+x_fit = linspace(0,1,100);
+y_fit = polyval(x, x_fit);
+
+
+colormap(brewermap(1000,'Blues'))
+caxis([0 1])
+
+xlabel('Corr. Reference')
+ylabel('Abandance')
+title(sprintf('S3F: c = %.2f, p = %.2f >0.05*', round(c,1), round(p,1)))
+
+% abundance for S std
+% figure
+subplot(2,3,4)
+
+selstr = find(ismember(strains.type,[13:20])&strains.Abs_log2median~=0&strains.TFId~=1);
+xv= strains.max_corr(selstr);
+yv = cellfun(@mean,strains.Abs_std(selstr));
+
+hold on
+scatter(xv,yv,40,[0.2588    0.5725    0.7765],'filled')
+[c,p] = corr(xv,yv);
+[x,y] = polyfit(xv,yv,1);
+x_fit = linspace(0,1,100);
+y_fit = polyval(x, x_fit);
+
+colormap(brewermap(1000,'Blues'))
+caxis([0 1])
+
+xlabel('Corr. Reference')
+ylabel('Abandance')
+title(sprintf('S1E: c = %.2f, p = %.2f >0.05*', round(c,1), round(p,1)))
+subplot(2,3,5)
+
+selstr = find(ismember(strains.type,[31:36])&strains.Abs_log2median~=0&strains.TFId~=1);
+xv= strains.max_corr(selstr);
+yv = cellfun(@mean,strains.Abs_std(selstr));
+
+hold on
+scatter(xv,yv,40,[0.2588    0.5725    0.7765],'filled')
+[c,p] = corr(xv,yv);
+[x,y] = polyfit(xv,yv,1);
+x_fit = linspace(0,1,100);
+y_fit = polyval(x, x_fit);
+
+
+colormap(brewermap(1000,'Blues'))
+caxis([0 1])
+
+xlabel('Corr. Reference')
+ylabel('Abandance')
+title(sprintf('S3F: c = %.2f, p = %.2f >0.05*', round(c,1), round(p,1)))
+
+subplot(2,3,6)
+
+selstr = find(ismember(strains.type,30)&strains.Abs_log2median~=0&strains.TFId~=1);
+xv= strains.max_corr(selstr);
+yv = cellfun(@mean,strains.Abs_std(selstr));
+
+hold on
+scatter(xv,yv,40,[0.2588    0.5725    0.7765],'filled')
+[c,p] = corr(xv,yv);
+[x,y] = polyfit(xv,yv,1);
+x_fit = linspace(0,1,100);
+y_fit = polyval(x, x_fit);
+
+
+colormap(brewermap(1000,'Blues'))
+caxis([0 1])
+
+xlabel('Corr. Reference')
+ylabel('Abandance')
+title(sprintf('S5C: c = %.2f, p = %.2f >0.05*', round(c,1), round(p,1)))
+
+
+%% FigS5C abudnace and corrDBD corrFL
+figure
+hold on
+tempsel = find(denovoinfo.abd~=0);
+scatter(denovoinfo.xRef(tempsel),denovoinfo.abd(tempsel),40,denovoinfo.xDBD(tempsel),'filled')
+[x,y] = polyfit(denovoinfo.xRef(tempsel),denovoinfo.abd(tempsel),1);
+
+x_fit = linspace(0,1,100);
+y_fit = polyval(x, x_fit);
+plot(x_fit, y_fit, '-', 'color',[.5 .5 .5],'LineWidth', 2);
+
+colormap(brewermap(1000,'Blues'))
+caxis([0 1])
+colorbar
+xlabel('Corr. Reference')
+ylabel('Abandance')
+title('c = 0')
+
+
